808387e2c70d9d3236831436e09a4e014cab1af5
Ville <ville-poikela@hotmail.com>
2026-08-23 11:18:20 +0300

    adds rest endpoint GET /blogs/:id

--- blogapp/server/controllers/blogs.js
+++ blogapp/server/controllers/blogs.js
@@ -8,6 +8,14 @@ blogsRouter.get("/", async (req, res) => {
   res.json(blogs)
 })
 
+blogsRouter.get("/:id", async (req, res) => {
+  const blogs = await Blog.findById(req.params.id).populate("user", {
+    username: 1,
+    name: 1,
+  })
+  res.json(blogs)
+})
+
 blogsRouter.post("/", middleware.userExtractor, async (req, res) => {
   const body = req.body
   const user = req.user
